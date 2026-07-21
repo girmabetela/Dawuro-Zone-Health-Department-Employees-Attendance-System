6db4e8ef19151f215b1810f4a878f492a98665f7
girmabetela <girmabetela119@gmail.com>
2026-07-21 17:01:23 +0300

    Create Login.jsx

---
+++ src/components/Login.jsx
@@ -0,0 +1,25 @@
+function Login() {
+  return (
+    <div>
+      <h2>Admin Login</h2>
+
+      <input 
+        type="text" 
+        placeholder="Username"
+      />
+
+      <br />
+
+      <input 
+        type="password" 
+        placeholder="Password"
+      />
+
+      <br />
+
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
